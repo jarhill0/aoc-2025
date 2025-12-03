101671f2bc18fcd8331201cf2d47391909d0561e
jarhill0 <git@reeshill.net>
2025-12-02 23:32:58 -0600

    Day 3 part 2

--- src/solutions/day03.rs
+++ src/solutions/day03.rs
@@ -1,35 +1,41 @@
 use crate::solutions::Solution;
 use crate::util::int;
-use std::fs::write;
 
 pub struct Day {}
 
-fn max_joltage(bank: &str) -> i64 {
+fn max_joltage(bank: &str, num_batteries: usize) -> i64 {
     let batteries: Vec<i64> = bank.chars().map(|c| int(&c.to_string()).unwrap()).collect();
     let len = batteries.len();
-    let first_digit = batteries[..len - 1]
-        .iter()
-        .max() // max_by_key returns rightmost maximum (annoying)
-        .unwrap();
-    let first_i = batteries
-        .iter()
-        .enumerate()
-        .filter(|x| x.1 == first_digit)
-        .next()
-        .unwrap()
-        .0;
-    let second_digit = batteries[first_i + 1..].iter().max().unwrap();
-    first_digit * 10 + second_digit
+    let mut joltage = 0;
+    let mut start_i = 0;
+    for d in 1..=num_batteries {
+        let best_digit = batteries[start_i..len - (num_batteries - d)]
+            .iter()
+            .max() // max_by_key returns rightmost maximum (annoying)
+            .unwrap();
+        let digit_i = batteries[start_i..]
+            .iter()
+            .enumerate()
+            .filter(|x| x.1 == best_digit)
+            .next()
+            .unwrap()
+            .0;
+        joltage *= 10;
+        joltage += best_digit;
+        start_i += digit_i + 1; // digit_i is relative to the (potentially already shortened) start_i
+    }
+    joltage
 }
 
 impl Solution for Day {
     fn part1(&self, input: &str) -> String {
-        let ans: i64 = input.lines().map(max_joltage).sum();
+        let ans: i64 = input.lines().map(|bank| max_joltage(bank, 2)).sum();
         format!("{}", ans)
     }
 
     fn part2(&self, input: &str) -> String {
-        "".to_string()
+        let ans: i64 = input.lines().map(|bank| max_joltage(bank, 12)).sum();
+        format!("{}", ans)
     }
 }
 
@@ -50,7 +56,7 @@ mod tests {
         assert_eq!(result1, "357");
 
         let result2 = d.part2(EXAMPLE_INPUT);
-        assert_eq!(result2, "");
+        assert_eq!(result2, "3121910778619");
     }
 
     #[test]
@@ -59,10 +65,9 @@ mod tests {
         let input = crate::input(3);
 
         let result1 = d.part1(&input);
-        assert_ne!(result1, "17298");
-        assert_eq!(result1, "");
+        assert_eq!(result1, "17452");
 
         let result2 = d.part2(&input);
-        assert_eq!(result2, "");
+        assert_eq!(result2, "173300819005913");
     }
 }
