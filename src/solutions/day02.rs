0ce089945ecd770a27dc1f025ecae67d2fd8b580
jarhill0 <git@reeshill.net>
2025-12-02 23:49:06 -0600

    Use regular expressions for day 2
    This is actually slower than my original approach. It takes almost 3
    seconds on my input. I assume there's just a bunch of backtracking.

--- src/solutions/day02.rs
+++ src/solutions/day02.rs
@@ -1,110 +1,34 @@
-use std::collections::HashSet;
+use fancy_regex::Regex;
 
 use crate::solutions::Solution;
 use crate::util::int;
 
 pub struct Day {}
 
-fn sum_invalids(range: Vec<&str>) -> i64 {
-    let low = int(range.get(0).unwrap()).unwrap();
-    let high = int(range.get(1).unwrap()).unwrap();
-    let d = digits(high) / 2;
-
-    let mut rpt_portion = low;
-    for _ in 0..d {
-        rpt_portion /= 10;
-    }
-    let rpt_portion = rpt_portion;
-
-    let mut total = 0;
-    for repeat_digits in rpt_portion.. {
-        if digits(repeat_digits) > d {
-            break;
-        }
-        if digits(repeat_digits) < d {
-            continue;
-        }
-        let mut candidate = repeat_digits;
-        for _ in 0..d {
-            candidate *= 10
-        }
-        candidate += repeat_digits;
-        let candidate = candidate;
-
-        if candidate > high {
-            break;
-        }
-        if low <= candidate {
-            total += candidate
-        }
-    }
-    total
-}
-
-fn digits(mut x: i64) -> i64 {
-    if x < 0 {
-        x = -x
-    }
-    let mut count = 0;
-    while x > 0 {
-        count += 1;
-        x /= 10;
-    }
-    count
-}
-
-fn sum_invalids2(invalids: &HashSet<i64>, range: Vec<&str>) -> i64 {
+fn sum_invalids(range: Vec<&str>, re: &Regex) -> i64 {
     let low = int(range.get(0).unwrap()).unwrap();
     let high = int(range.get(1).unwrap()).unwrap();
 
-    (low..=high).filter(|x| invalids.contains(x)).sum()
-}
-
-fn digit_left_shift(mut x: i64, by: i64) -> i64 {
-    for _ in 0..by {
-        x *= 10;
-    }
-    x
-}
-
-fn all_invalids2(n: i64) -> HashSet<i64> {
-    let mut set = HashSet::new();
-
-    for d in 1..=n {
-        for rpt_len in 1..=(d / 2) {
-            if d % rpt_len != 0 {
-                continue;
-            }
-
-            let segment_start = digit_left_shift(1, rpt_len - 1); // e.g. 100
-            let segment_end = digit_left_shift(1, rpt_len) - 1; // e.g. 999
-            for segment in segment_start..=segment_end {
-                let mut candidate = 0;
-                for _ in 0..(d / rpt_len) {
-                    candidate = digit_left_shift(candidate, rpt_len) + segment
-                }
-                set.insert(candidate);
-            }
-        }
-    }
-
-    set
+    (low..=high)
+        .filter(|x| re.is_match(&format!("{}", x)).unwrap_or(false))
+        .sum()
 }
 
 impl Solution for Day {
     fn part1(&self, input: &str) -> String {
+        let re = Regex::new(r"^(\d+)\1$").unwrap();
         let ans: i64 = input
             .split(',')
-            .map(|range| sum_invalids(range.split('-').collect()))
+            .map(|range| sum_invalids(range.split('-').collect(), &re))
             .sum();
         format!("{}", ans)
     }
 
     fn part2(&self, input: &str) -> String {
-        let invalids = all_invalids2(12); // this is really silly lmao
+        let re = Regex::new(r"^(\d+)\1+$").unwrap();
         let ans: i64 = input
             .split(',')
-            .map(|range| sum_invalids2(&invalids, range.split('-').collect()))
+            .map(|range| sum_invalids(range.split('-').collect(), &re))
             .sum();
         format!("{}", ans)
     }
