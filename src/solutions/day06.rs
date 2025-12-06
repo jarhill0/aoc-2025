df80b82e7cea33a05f7ad932a12d6f37515de49b
jarhill0 <git@reeshill.net>
2025-12-05 23:37:59 -0600

    Day 6 part 2 (as submitted)

--- src/solutions/day06.rs
+++ src/solutions/day06.rs
@@ -1,5 +1,5 @@
 use crate::solutions::Solution;
-use crate::util::{int, token_lines};
+use crate::util::{int, token_lines, tokens};
 
 pub struct Day {}
 
@@ -22,8 +22,52 @@ impl Solution for Day {
     }
 
     fn part2(&self, input: &str) -> String {
-        "".to_string()
+        let mut lines: Vec<&str> = input.lines().collect();
+        let operators = tokens(lines[lines.len() - 1]);
+        lines.remove(lines.len() - 1);
+        let re_parsed = re_parse(&lines);
+
+        let mut op_ind = 0;
+        let mut total = 0;
+        let mut nums = Vec::new();
+        re_parsed.iter().for_each(|n| {
+            dbg!(n);
+            if let Ok(n) = int(n.trim()) {
+                nums.push(n);
+            } else {
+                total += prob(&nums, operators[op_ind]);
+                nums.clear();
+                op_ind += 1;
+                dbg!("adding it up!", op_ind);
+            }
+        });
+        if nums.len() > 0 {
+            total += prob(&nums, operators[op_ind]);
+        }
+
+        format!("{}", total)
     }
+}
+
+fn prob(nums: &Vec<i64>, op: &str) -> i64 {
+    match op {
+        "*" => nums.iter().product(),
+        "+" => nums.iter().sum(),
+        _ => panic!("unknown operation"),
+    }
+}
+
+fn re_parse(orig: &Vec<&str>) -> Vec<String> {
+    let mut out = Vec::new();
+    let width = orig.iter().map(|x| x.len()).max().unwrap();
+    for origc in (0..width) {
+        let mut out_row = Vec::new();
+        for origr in (0..orig.len()) {
+            out_row.push(orig[origr].chars().nth(origc).unwrap_or(' '))
+        }
+        out.push(out_row.iter().collect())
+    }
+    out
 }
 
 #[cfg(test)]
@@ -43,7 +87,7 @@ mod tests {
         assert_eq!(result1, "4277556");
 
         let result2 = d.part2(EXAMPLE_INPUT);
-        assert_eq!(result2, "");
+        assert_eq!(result2, "3263827");
     }
 
     #[test]
